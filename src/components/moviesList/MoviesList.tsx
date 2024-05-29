d9633a578496362129202cdbf498705598559d59
Lakshmi Kamjula <lkamjula@osius.com>
2024-05-29 18:18:52 +0530

    update

--- src/components/moviesList/MoviesList.tsx
+++ src/components/moviesList/MoviesList.tsx
@@ -9,16 +9,15 @@ const MoviesList = memo((props: movieListPropType) => {
   return (
     <div>
       <h1 className="movie-list__heading">
-        {data[0].release_date.substring(0, 4)}
+        {data[0]?.release_date?.substring(0, 4)}
       </h1>
       <div className="movie-list">
         {data?.map((item: Record<string, any>) => {
           return (
             <MovieCard
-              title={item.title}
-              poster_path={item.poster_path}
-              release_date={item.release_date}
-              overview={item.overview}
+              title={item?.title}
+              poster_path={item?.poster_path}
+              overview={item?.overview}
               vote_average={item.vote_average}
               key={item.id}
             />
--- src/types/movieCardType.ts
+++ src/types/movieCardType.ts
@@ -1,7 +1,6 @@
 export type MovieCardType={
     title:string,
     poster_path:string,
-    release_date:string,
     overview:string,
     vote_average:number,
     key:number
